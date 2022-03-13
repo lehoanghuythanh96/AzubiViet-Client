d1e9d856c8cf37d0ecbf263ca338abcc27638025
huythanh <lehoanghuythanh96@gmail.com>
2022-03-13 19:17:21 +0100

    Added sample environment files

---
+++ src/environments/sample-environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    BASE_SOCKET_URL: "https://...", // Socket IO server
+    BASE_CDN_URL: "http://...", // CDN url
+    BASE_API_URL: "http://...", // Database web server
+    GG_CLIENT_ID: "", // GG login Client ID
+    RECAPTCHA_SITE_KEY: "" // Recaptcha Sitekey
+};
